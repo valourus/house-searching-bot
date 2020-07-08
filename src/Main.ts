de43d6a8baa04c957dc702e892b9c4f3380843ea
koenvanderels <koen.vanderels@eonics.nl>
2020-07-08 12:46:43 +0200

    fix

--- src/Main.ts
+++ src/Main.ts
@@ -2,9 +2,11 @@ import { HouseSearchingBot } from "./HouseSearchingBot";
 import { config } from 'dotenv';
 
 config();
-async function main(): Promise<void> {
-    await HouseSearchingBot();
-}
+(async () => {
+    async function main(): Promise<void> {
+        await HouseSearchingBot();
+    }
 
-setInterval(()=> main(), (1000 * 60) * Number(process.env.CHECK_INTERVAL_IN_MIN));
-main();
+    setInterval(() => main(), ( 1000 * 60 ) * Number(process.env.CHECK_INTERVAL_IN_MIN));
+    await main();
+})();
